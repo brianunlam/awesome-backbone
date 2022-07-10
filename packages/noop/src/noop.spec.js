67f659ae7d3c899f67e9efe9c6092a3129211587
Brian Alonso <brian.alonso.a@gmail.com>
2022-07-10 03:50:26 -0300

    Create next app and import internal component

---
+++ packages/noop/src/noop.spec.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const _1 = require("./");
+describe('doSomething', () => {
+    it('should return true', () => {
+        expect((0, _1.noop)()).toBe(undefined);
+    });
+});
