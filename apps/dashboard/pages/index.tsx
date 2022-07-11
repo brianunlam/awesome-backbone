b784afb68e4538bfc5ace25175a22d99dd7b97bf
Hugo Venega <hugo.venega.00@gmail.com>
2022-07-11 15:36:53 -0300

    Add Thing component to index

--- apps/dashboard/pages/index.tsx
+++ apps/dashboard/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { Thing } from 'components'
+import { Thing } from '@hugovng/components'
 
 const Home: NextPage = () => {
   return (
