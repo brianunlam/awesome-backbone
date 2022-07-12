e047de03b909b4f808595bd8e74e79e83113c8bc
Hugo Venega <86172480+hugovenega@users.noreply.github.com>
2022-07-12 14:33:46 -0300

    Add headless UI (#6)
    * Add Headless UI dependency and create toggleSwitch component with its storie
    
    * Fix lint

---
+++ packages/components/stories/toggleSwitch.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { ToggleSwitch, ToggleSwitchProps } from '../src/toggleSwitch';
+
+const meta: Meta = {
+  title: 'Input/Toggle Switch',
+  component: ToggleSwitch,
+  argTypes: {
+    onClick: { action: 'clicked' },
+    enabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+  parameters: {
+    controls: { expanded: true },
+  },
+};
+
+export default meta;
+
+const Template: Story<ToggleSwitchProps> = function (args) {
+  return <ToggleSwitch {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  enabled: false,
+};
