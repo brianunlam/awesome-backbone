405741d292d9d1d15889fb05f43134fd356b6cce
Hugo Venega <hugo.venega.00@gmail.com>
2022-07-12 17:21:09 -0300

    Fix test error

--- apps/dashboard/pages/index.tsx
+++ apps/dashboard/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { ToDo } from 'to-do'
+import { Thing } from 'components'
 
 const Home: NextPage = () => {
   return (
@@ -17,8 +17,7 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <ToDo>vamooos neweelllssss</ToDo>
-
+        <Thing />
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
--- packages/components/tailwind.config.js
+++ packages/components/tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
 }
