20ec3b0d61345b921262731afb5144bb9858047b
Hugo Venega <hugo.venega.00@gmail.com>
2022-07-15 14:44:01 -0300

    Add sass style and tailwind css

--- packages/components/src/Card/Card.spec.tsx
+++ packages/components/src/Card/Card.spec.tsx
@@ -5,7 +5,16 @@ import { Default as Card } from './Card.stories';
 describe('Card', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Card classes=" h-64 w-64" variant='elevation' />, div);
+    ReactDOM.render(
+      <Card
+        username="Pugliese"
+        image="https://img.freepik.com/vector-gratis/pato-lindo-blanco_1308-41058.jpg?w=360"
+        rate="8.1"
+        date="16 Mar 2021"
+        review="A lot of text"
+      />,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
--- packages/components/src/Card/Card.tsx
+++ packages/components/src/Card/Card.tsx
@@ -1,25 +1,35 @@
+import '../../tailwind.css';
+import '../css/style.comp.css';
+
 import React, { FC, HTMLAttributes } from 'react';
 
 export interface CardProps extends HTMLAttributes<HTMLDivElement> {
-  variant: 'elevation' | 'outlined';
-  classes: string;
+  username: string;
+  image: string;
+  rate: string;
+  date: string;
+  review: string;
 }
 export const Card: FC<CardProps> = function ({
-  variant = 'elevation',
-  children,
-  classes,
-  ...rest
+  username,
+  image,
+  rate,
+  date,
+  review,
 }) {
   return (
-    <div
-      className={[
-        classes,
-        'rounded-xl p-2',
-        `${variant === 'elevation' ? 'shadow-md' : 'border-2 border-solid'}`,
-      ].join(' ')}
-      {...rest}
-    >
-      {children}
+    <div className="user-reviews rounded-md  border-2 border-y-gray-400">
+      <figure className="review">
+        <blockquote className="review__text">{review}</blockquote>
+        <figcaption className="review__user">
+          <img src={image} alt="User 1" className="review__photo" />
+          <div className="review__user-box">
+            <p className="review__user-name">{username}</p>
+            <p className="review__user-date">{date}</p>
+          </div>
+          <div className="review__rating">{rate}</div>
+        </figcaption>
+      </figure>
     </div>
   );
 };
